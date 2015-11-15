7095d74d7fb87469c4abb7036f68bba2094c9d01
Ashan Marla <ashan.marla@sbcglobal.net>
2015-11-14 17:26:11 -0800

    Initial Commit

---
+++ HackSC2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackSC2015
+//
+//  Created by Ashan Marla on 11/14/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HackSC2015Tests/HackSC2015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HackSC2015Tests.swift
+//  HackSC2015Tests
+//
+//  Created by Ashan Marla on 11/14/15.
+//
+//
+
+import XCTest
+@testable import HackSC2015
+
+class HackSC2015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
